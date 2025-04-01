1bb1e635fe1a523d4df95472d3eef6c6361a1bb8
PJaworski <pjaworski.dev@gmail.com>
2025-04-01 13:51:56 +0200

    Added bool, if statements

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,26 @@ public class Main {
        // declaration
         int age;
         double agedoub;
+        char grade;
 
         // assignment
-        age = 25;
-        agedoub = 25.5;
+        age = 26;
+        agedoub = -25.5;
+        grade = 'A';
 
         // sout
-        System.out.println(age);
-        System.out.println(agedoub);
+        System.out.println(grade);
 
+        boolean isStudent = true;
+
+        System.out.println(isStudent);
+
+        // if statements
+        if(isStudent == true) {
+            System.out.println("You're a student");
+        }
+        else {
+            System.out.println("Buy IDEA");
+        }
     }
 }
