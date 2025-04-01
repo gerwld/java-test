4b76a803855efaf0b2bd35b69e64bdf0b7f47ef3
PJaworski <pjaworski.dev@gmail.com>
2025-04-01 15:56:17 +0200

    Used java.util.Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        /*
-        primitives: STACK(double int char boolean) - stored in memory
-        reference values: HEAP(string, array, object) - memory access, not written directly in memory
-         */
+        // get the name of a user
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine(); //.next if you want to avoid spaces
 
-        // string is a series of chars, that's why its heap
-        String name = "some string";
-        String food = "another one";
-        String email = "pjaworskidev@gmail.com";
+        // get the age of a user
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Email: " + email);
+        System.out.println("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
+        double gpa = 0;
+        if(isStudent) {
+            // get the gpa of a user
+            System.out.print("Enter your GPA: ");
+            gpa = scanner.nextDouble();
+        }
+
+        // output the data
+        System.out.println("Hello, " + name);
+        System.out.println("You are " + age + " years old.");
+        if(isStudent) {
+            System.out.println("You are a student and your GPA is: " + gpa);
+        }
+
+        scanner.close();
     }
 }
