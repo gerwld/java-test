b868b75388e0fe1d7d0154deaddb5a38d7ab95c5
PJaworski <pjaworski.dev@gmail.com>
2025-04-01 18:39:29 +0200

    do-while

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-     for(int i = 0; i < 100; i++) {
-         System.out.println(i);
-     }
+    Scanner scanner = new Scanner(System.in);
+    int value;
+    do {
+        System.out.print("Введи 5: ");
+        value = scanner.nextInt();
+    }
+    while(value!=5);
 
+    System.out.println("Цифра 5 введена успешно");
 
     }
 }
