9bac9021dc3ddc38a93243c7db332907cfd1d9c9
PJaworski <pjaworski.dev@gmail.com>
2025-04-01 18:12:46 +0200

    cycle while

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,13 @@
-import java.util.Random;
+
 
 public class Main {
     public static void main(String[] args) {
-        // random
-        Random random = new Random();
 
-        double number;
-        double number2;
-        double number3;
+        int i = 5;
 
-        number = random.nextDouble(0,1);
-        number2 = random.nextDouble(0,1);
-        number3 = random.nextDouble(0,1);
-
-        boolean isHeads;
-
-        isHeads = random.nextBoolean();
-
-        System.out.println(number);
-        System.out.println(number2);
-        System.out.println(number3);
-
-
-        System.out.println(isHeads ? "Heads" : "Tails");
-
-
+        while(i>0) {
+            i--;
+            System.out.println(i);
+        }
     }
 }
