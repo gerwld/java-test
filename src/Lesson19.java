5d086f432a33b8190482a5e88ce680d6a12bdb1e
PJaworski <pjaworski.dev@gmail.com>
2025-04-02 20:30:21 +0200

    constructors, overloading

--- src/Lesson19.java
+++ src/Lesson19.java
@@ -1,16 +1,24 @@
 public class Lesson19 {
     public static void main(String[] args) {
-        Human human1 = new Human();
-        human1.setAge(19);
-        human1.setName("Adam");
-
+        Human human1 = new Human("Adam", 22);
         human1.getInfo();
     }
 }
 
 class Human {
-    String name;
-    int age;
+    private String name = "$username";
+    private int age;
+
+    // пример перегрузки методов, используется тот чьи параметры пришли
+    public Human(int age) {
+        System.out.println("constructor 1 init call");
+        this.age = age;
+    }
+    public Human(String name, int age) { // constructor как в джаваскрипт constructor()
+        System.out.println("constructor 2 init call");
+        this.name = name;
+        this.age = age;
+    }
 
     // Setters
     public void setAge(int age) {
