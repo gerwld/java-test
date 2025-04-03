28026c8d5e7ee6ffccdf65835158e65a1e5c5b52
PJaworski <pjaworski.dev@gmail.com>
2025-04-03 11:30:42 +0200

    inheritance example

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating.");
+    }
+
+    public void bark() {
+        System.out.println("Woof.");
+    }
+}
---
+++ src/Lesson25.java
@@ -0,0 +1,14 @@
+public class Lesson25 {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+
+        animal.sleep();
+        animal.eat();
+
+        System.out.println();
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
